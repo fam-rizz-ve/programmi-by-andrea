3f0a17683a89c8d14e9113f609b225cbbacf267a
fam-rizz-ve <fam.rizz.ve@gmail.com>
2024-07-16 14:41:48 +0200

    spero la fine del installer

--- progetti ibridi/installer/dipendenze.bat
+++ progetti ibridi/installer/dipendenze.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
 PowerShell -NoProfile -Command "pip install requests"
 PowerShell -NoProfile -Command "pip install pywin32 winshell"
-start menu-dowload-servizi.exe
+PowerShell -NoProfile -Command "pip install winshell"
+PowerShell -NoProfile -Command "pip install pywin32"
+start improved-service-installer.exe
